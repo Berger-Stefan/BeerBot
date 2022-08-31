864a0237cac2fbb48495c35defcdb24129579f83
berger.st.11.11@gmail.com <berger.st.11.11@gmail.com>
2022-08-31 11:53:07 +0200

    renamed template file

--- ros2_code/src/template_code/src/ServoTemplate.cpp
+++ ros2_code/src/template_code/src/ServoTemplate.cpp
@@ -8,14 +8,20 @@ using std::placeholders::_1;
 #define SERVO_NAME "M1"
 //TODO define servo pins up here and attach the servo inside the constructor
 
-class TemplateServo : public rclcpp::Node
+class ServoTemplate : public rclcpp::Node
 {
 public:
-  TemplateServo()
-  : Node("template servo")
+  ServoTemplate()
+  : Node("servo_template")
   {
+    RCLCPP_INFO(this->get_logger(), "Motor %s node created", SERVO_NAME);
     subscription_ = this->create_subscription<custom_interfaces::msg::ServoData>(
-      SERVO_NAME, 10, std::bind(&TemplateServo::topic_callback, this, _1));
+      SERVO_NAME, 10, std::bind(&ServoTemplate::topic_callback, this, _1));
+  }
+
+  ~ServoTemplate()
+  {
+    RCLCPP_INFO(this->get_logger(), "Motor %s node destroyed", SERVO_NAME);
   }
 
 private:
@@ -23,7 +29,7 @@ private:
   {
     //TODO here move servo with the given value from function parameter
     
-    RCLCPP_INFO(this->get_logger(), "I heard: '%d'", msg->position);
+    RCLCPP_INFO(this->get_logger(), "Set motor %s positon to: '%d'", SERVO_NAME, msg->position);
   }
 
   rclcpp::Subscription<custom_interfaces::msg::ServoData>::SharedPtr subscription_;
@@ -32,7 +38,7 @@ private:
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<TemplateServo>());
+  rclcpp::spin(std::make_shared<ServoTemplate>());
   rclcpp::shutdown();
   return 0;
 }
