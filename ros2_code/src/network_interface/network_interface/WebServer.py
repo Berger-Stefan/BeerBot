0ac6c28a1b11f7e69a4727dce5880776af31b855
berger.st.11.11@gmail.com <berger.st.11.11@gmail.com>
2022-09-06 21:18:02 +0200

    add more strucutre

---
+++ ros2_code/src/network_interface/network_interface/WebServer.py
@@ -0,0 +1,18 @@
+from imutils.video import VideoStream
+from flask import Response
+from flask import Flask
+from flask import render_template
+import threading
+import argparse
+import datetime
+import imutils
+import time
+import cv2
+
+
+def main():
+    print('Hi from network_interface.')
+
+
+if __name__ == '__main__':
+    main()
